ba91e8d27826319fb9708f6b2dacff40959f9aa4
Basis1977 <55097424+Basis1977@users.noreply.github.com>
2022-01-30 17:42:00 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    subscription_id = "785f9183-24d6-48f8-96b1-17a654db8775"
     resource_group_name  = "Free Trial"
     storage_account_name = "azstore16789"
     container_name       = "tfstatefile"
