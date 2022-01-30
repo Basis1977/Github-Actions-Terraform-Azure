041bee614583797ef121ca8beea721ad3e137eba
Basis1977 <55097424+Basis1977@users.noreply.github.com>
2022-01-30 12:06:19 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
+    resource_group_name  = "ResourceGroup1lod21355429"
+    storage_account_name = "terraformdevopsaz"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
